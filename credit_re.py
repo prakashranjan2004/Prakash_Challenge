1b455719007ea3edfeaf1dd10035e595d53cc7be
root <prakashranjan2004@gmail.com>
2020-04-28 16:09:31 -0400

    abc

--- credit_re.py
+++ credit_re.py
@@ -1,5 +1,6 @@
 #Note:-
 # to test git on 04282020
+# to test git on 04282020_2
 # Condition - "It must NOT have 4 or more consecutive repeated digits" is not satsfied with this program. 
 # To satisfy above condition, need other program. With the help of google I found some program and after some tweakings
 # that's working as well for me. Not sharing that because it's not my original work.
